47a1488ab9ae44ad2162a3b80046d3318769f2a2
seahmin <msa0309@gmail.com>
2017-07-21 21:45:48 -0800

    show MAC Address

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/ioctl.h>
 
 int main(int argc, char *argv[])
 {
     pcap_t *handle;			/* Session handle */
-    char *dev;			/* The device to sniff on */
+    int res;
+    char *dev="eth0";			/* The device to sniff on */
     char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-    struct bpf_program fp;		/* The compiled filter */
+    struct bpf_program *fp;		/* The compiled filter */
     char filter_exp[] = "port 80";	/* The filter expression THIS ONE ONLY CATCHES PORT HTTP*/
     bpf_u_int32 mask;		/* Our netmask */
     bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr header;	/* The header that pcap gives us */
+    struct pcap_pkthdr *header;	/* The header that pcap gives us */
     const u_char *packet;		/* The actual packet */
 
     /* Define the device */
@@ -20,17 +23,20 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
     return(2);
     }
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+    handle = pcap_open_live("eth0", BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL)
     {
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return(2);
     }
     printf("Device: %s\n", dev);
-    while(1)
-    {
-        packet = pcap_next(handle, &header);
-        printf("1111\n");
-    }
-}
 
+    //while((
+    if(res=pcap_next_ex(handle, &header, &packet)==1){
+       // if(res==0) continue;
+        printf("Source MAC Address : %02x:%02x:%02x:%02x:%02x:%02x\n", *(packet+6), *(packet+7), *(packet+8), *(packet+9), *(packet+10), *(packet+11));
+        printf("Destination MAC Address : %02x:%02x:%02x:%02x:%02x:%02x\n", *(packet), *(packet+1), *(packet+2), *(packet+3), *(packet+4), *(packet+5));
+    }
+    pcap_close(handle);
+    return 0;
+}
